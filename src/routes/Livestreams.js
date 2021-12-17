10c3768f7aff8dda3edd1892430c7f64322ead36
luongch <christopher_luong@Live.ca>
2021-12-17 17:46:08 -0500

    separate upcoming into it's own component

--- src/routes/Livestreams.js
+++ src/routes/Livestreams.js
@@ -1,38 +1,24 @@
 import React, { useState, Fragment } from 'react';
+import Upcoming from './Upcoming'
 
 const Livestreams = () => {
   const [isLiveLoading, setIsLiveLoading] = useState(true);
-  const [isUpcomingLoading, setIsUpcomingLoading] = useState(true);
   const [isError, setIsError] = useState(false);
   const [live, setLive] = useState([]);
-  const [upcoming, setUpcoming] = useState([]);
 
   React.useEffect(() => {
       getLiveStreams();
-      getUpcoming();
   }, []);
 
   const getLiveStreams = async () => {
-    const response = await fetch("/api/v1/videos/live")
+    const response = await fetch("/api/v1/videos/live")    
     const livestreams = await response.json();
-    if(livestreams.success) {
+    if(response.status === 200) {
       setIsLiveLoading(false);
       setLive(livestreams.data)
     }
     else {
       setIsLiveLoading(false);
-      setIsError(true);
-    }    
-  }
-  const getUpcoming = async () => {
-    const response = await fetch("/api/v1/videos/upcoming");
-    const upcoming = await response.json();
-    if(upcoming.success) {
-      setIsUpcomingLoading(false);
-      setUpcoming(upcoming.data)
-    }
-    else {
-      setIsUpcomingLoading(false);
       setIsError(true);
     }    
   }
@@ -46,34 +32,34 @@ const Livestreams = () => {
       )
     }
     
-    const Livestream = (props) => {
-      const {title,id,author} = props.livestream;
-      return (
-        <li>
-          <h1>{title}</h1>
-          <h2>{author}</h2>
-          <a href={`https://www.youtube.com/watch?v=${id}`}>{`https://www.youtube.com/watch?v=${id}`}</a>
-        </li>
-      );
-    }
-    if(isError) {
-      return(
-        <h2>Error...</h2>
-      )
-    }
+  const Livestream = (props) => {
+    const {title,id,author} = props.livestream;
     return (
-      <Fragment>
-        <>{isLiveLoading ? <h2>Loading livestreams...</h2> : <LivestreamList livestreams={live} />}</>        
-        <div style={{
-            borderBottom: "solid 1px",
-            paddingBottom: "1rem"
-        }}></div>
-        <>{isUpcomingLoading ? <h2>Loading upcoming...</h2> : <LivestreamList livestreams={upcoming} />}</>        
-      </Fragment>
-      
-      
-      
-    )     
+      <li>
+        <h1>{title}</h1>
+        <h2>{author}</h2>
+        <a href={`https://www.youtube.com/watch?v=${id}`}>{`https://www.youtube.com/watch?v=${id}`}</a>
+        <iframe width="420" height="315"
+          src={`https://www.youtube.com/embed/${id}`}>
+        </iframe>
+      </li>
+    );
+  }
+  if(isError) {
+    return(
+      <h2>Error...</h2>
+    )
+  }  
+  return (
+    <Fragment>
+      <>{isLiveLoading ? <h2>Loading livestreams...</h2> : <LivestreamList livestreams={live} />}</>        
+      <div style={{
+          borderBottom: "solid 1px",
+          paddingBottom: "1rem"
+      }}></div>
+      <Upcoming></Upcoming>
+    </Fragment>
+  )     
 }
 
 export default Livestreams;
---
+++ src/routes/Upcoming.js
@@ -0,0 +1,58 @@
+import React, { useState, Fragment } from 'react';
+
+const Upcoming = () => {
+    const [upcoming, setUpcoming] = useState([])
+    const [isUpcomingLoading, setIsUpcomingLoading] = useState(true);
+    const [isError, setIsError] = useState(false);
+
+    React.useEffect(() => {
+        getUpcoming();
+    }, []);
+
+    const getUpcoming = async () => {
+        let response = await fetch("/api/v1/videos/upcoming")
+        let upcoming = await response.json();
+
+        if(response.status === 200) {
+            setIsUpcomingLoading(false);
+            setUpcoming(upcoming.data)
+        }
+    }
+
+    const UpcomingStreamList = ({upcomingStreams}) => {
+        const upcomingStreamsItems = upcomingStreams.map((upcoming)=>{
+            return (
+                <UpcomingStream key={upcoming._id} upcoming={upcoming}></UpcomingStream>
+            )
+        })
+        return (
+            <ul>{upcomingStreamsItems}</ul>
+          )
+    }
+
+    const UpcomingStream = ({title,id,author}) => {
+        return (
+            <li>
+              <h1>{title}</h1>
+              <h2>{author}</h2>
+              <a href={`https://www.youtube.com/watch?v=${id}`}>{`https://www.youtube.com/watch?v=${id}`}</a>
+              <iframe width="420" height="315"
+                src={`https://www.youtube.com/embed/${id}`}>
+              </iframe>
+            </li>
+          );
+    }
+
+    if(isError) {
+        return(
+          <h2>Error...</h2>
+        )
+      }
+      return (
+        <Fragment>
+          <>{isUpcomingLoading ? <h2>Loading upcoming...</h2> : <UpcomingStreamList upcomingStreams={upcoming} />}</>
+        </Fragment>
+      )    
+}
+
+export default Upcoming;
