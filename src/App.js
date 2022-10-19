fb0a99b10574e9b214895c81497ee0f341824c31
Chris Luong <christopher_luong@live.ca>
2022-10-19 17:31:45 -0400

    fix warning messages

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
-import React, {useState, useContext} from "react";
+import React, {useState} from "react";
+// import React, {useState, useContext} from "react";
 import { Outlet } from "react-router-dom";
 import Sidebar from "./components/Sidebar";
 import './styles/app.css';
 import Navbar from "./components/Navbar";
-import { globalContext } from './Context';
+// import { globalContext } from './Context';
 import axiosInstance from './api/axiosConfig';
 
 
 function App() {
-  const userObject = useContext(globalContext);
+  // const userObject = useContext(globalContext);
   const [showProfile, setShowProfile] = useState(false);
   const [sessionUser, setSessionUser] = useState({id:""});
   console.log("after setting sessionUser state", sessionUser);
@@ -17,22 +18,22 @@ function App() {
   const toggleProfile = () =>{ 
     setShowProfile(!showProfile);
   }
-  const getSession = async () => {
-    axiosInstance.get('/api/v1/session', {withCredentials:true})
-    .then((res)=>{
-      if(res.data) {
-        console.log("res.data", res.data.user)
-        setSessionUser(res.data.user)
-      }
-    })
-    // let response = await fetch(`${baseUrl}/api/v1/session`);
-    // let result = await response.json();  
+  // const getSession = async () => {
+  //   axiosInstance.get('/api/v1/session', {withCredentials:true})
+  //   .then((res)=>{
+  //     if(res.data) {
+  //       console.log("res.data", res.data.user)
+  //       setSessionUser(res.data.user)
+  //     }
+  //   })
+  //   // let response = await fetch(`${baseUrl}/api/v1/session`);
+  //   // let result = await response.json();  
     
-    // if(result.user) {
-    //   setSessionUser({...result.user})
-    // }
-    // console.log(sessionUser)
-  }
+  //   // if(result.user) {
+  //   //   setSessionUser({...result.user})
+  //   // }
+  //   // console.log(sessionUser)
+  // }
   
   console.log("sessionUser is", sessionUser)
   React.useEffect(()=>{
